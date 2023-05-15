40bc7220319dfaef50a2e992d5cf0a2deab7d5fd
Dewa <dewaasmara589@gmail.com>
2023-05-15 10:49:28 +0700

    Add Date Format & next page in PDF

--- app/src/main/java/com/example/pdf_itextpdf/MainActivity.java
+++ app/src/main/java/com/example/pdf_itextpdf/MainActivity.java
@@ -9,6 +9,8 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.ActivityNotFoundException;
 import android.content.ClipData;
 import android.content.Context;
@@ -23,6 +25,9 @@ import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.itextpdf.io.image.ImageData;
@@ -33,10 +38,12 @@ import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.borders.Border;
 import com.itextpdf.layout.borders.SolidBorder;
+import com.itextpdf.layout.element.AreaBreak;
 import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Image;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.property.AreaBreakType;
 import com.itextpdf.layout.property.TextAlignment;
 
 import java.io.ByteArrayOutputStream;
@@ -44,13 +51,18 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText etTimeDate;
     private Button btnPDF;
 
     private static final String TAG = "PdfCreatorActivity";
     public final int REQUEST_CODE = 100;
-    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,23 +70,61 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnPDF = (Button) findViewById(R.id.btnPDF);
-        context = this;
+        etTimeDate = (EditText) findViewById(R.id.etTimeDate);
+
+        // Edit text tanggal untuk etTglBerangkat
+        Calendar calendar = Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR,year);
+                calendar.set(Calendar.MONTH,month);
+                calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+
+                updateCalender();
+            }
+
+            private void updateCalender() {
+                // Mengatur format yang akan tampil di EditText untuk tanggal Periode
+                String format = "dd-MM-yyyy";
+                SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.US);
+
+                etTimeDate.setText(sdf.format(calendar.getTime()));
+            }
+        };
+
+        etTimeDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, date,
+                        calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH));
+                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+                datePickerDialog.show();
+            }
+        });
 
         btnPDF.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    if (ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE)
-                            == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
-                            getApplicationContext(), WRITE_EXTERNAL_STORAGE) ==
-                            PackageManager.PERMISSION_GRANTED)
-                    {
-                        createPdf();
-                    }else {
-                        requestAllPermission();
+                if (etTimeDate.getText().toString().trim().length() == 0){
+                    etTimeDate.setError("Tolong Diisi");
+                }else {
+                    etTimeDate.setError(null);
+                    try {
+                        if (ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE)
+                                == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                                getApplicationContext(), WRITE_EXTERNAL_STORAGE) ==
+                                PackageManager.PERMISSION_GRANTED)
+                        {
+                            createPdf();
+                        }else {
+                            requestAllPermission();
+                        }
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
                     }
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
                 }
             }
         });
@@ -82,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void createPdf() throws FileNotFoundException {
         File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
-                "/Tes15.pdf");
+                "/Tes6.pdf");
         OutputStream output = new FileOutputStream(file);
 
         PdfWriter pdfWriter = new PdfWriter(file);
@@ -234,8 +284,57 @@ public class MainActivity extends AppCompatActivity {
         table2.addCell(new Cell(1, 2).add(new Paragraph("Jumlah Pax : 25 Hari").setFontSize(9)).setBorder(Border.NO_BORDER));
 //        table2.addCell(new Cell(1, 2).add(new Paragraph("25 Hari").setFontSize(9)).setBorder(Border.NO_BORDER));
 
+        //Tabel 2 --- 09
+        table2.addCell(new Cell().add(new Paragraph("")).setBorder(Border.NO_BORDER));
+        table2.addCell(new Cell().add(new Paragraph("")).setBorder(Border.NO_BORDER));
+        table2.addCell(new Cell().add(new Paragraph("")).setBorder(Border.NO_BORDER));
+        table2.addCell(new Cell().add(new Paragraph("")).setBorder(Border.NO_BORDER));
+        table2.addCell(new Cell().add(new Paragraph("")).setBorder(Border.NO_BORDER));
+        table2.addCell(new Cell().add(new Paragraph("")).setBorder(Border.NO_BORDER));
+        table2.addCell(new Cell().add(new Paragraph("")).setBorder(Border.NO_BORDER));
+        table2.addCell(new Cell().add(new Paragraph("")).setBorder(Border.NO_BORDER));
+
+        //Tabel 2 --- 010
+        table2.addCell(new Cell().add(new Paragraph("")).setBorder(Border.NO_BORDER));
+        table2.addCell(new Cell().add(new Paragraph("")).setBorder(Border.NO_BORDER));
+        table2.addCell(new Cell().add(new Paragraph("")).setBorder(Border.NO_BORDER));
+        table2.addCell(new Cell().add(new Paragraph("")).setBorder(Border.NO_BORDER));
+        table2.addCell(new Cell().add(new Paragraph("")).setBorder(Border.NO_BORDER));
+        table2.addCell(new Cell().add(new Paragraph("")).setBorder(Border.NO_BORDER));
+        table2.addCell(new Cell().add(new Paragraph("")).setBorder(Border.NO_BORDER));
+        table2.addCell(new Cell().add(new Paragraph("")).setBorder(Border.NO_BORDER));
+
         document.add(table1);
         document.add(table2);
+
+        // Cetak Tanggal
+        int durasiWisata = 3;
+        String tanggal = etTimeDate.getText().toString().trim();
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        for (int i = 0; i<durasiWisata; i++){
+            try {
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(simpleDateFormat.parse(tanggal));
+                cal.add(Calendar.DAY_OF_MONTH,i);
+
+                String hasilTambahTanggal = String.valueOf(simpleDateFormat.format(cal.getTime()));
+
+                Paragraph paragraph = new Paragraph(hasilTambahTanggal).setFontSize(14).setBold();
+
+                document.add(paragraph);
+
+                if (i<durasiWisata-1){
+                    // Add next page PDF
+                    document.add(new AreaBreak(AreaBreakType.NEXT_PAGE));
+                }
+
+                Log.e(TAG, "Tanggal : " + simpleDateFormat.format(cal.getTime()).toString());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+
 
 
         // -------- END Content --------
@@ -248,7 +347,7 @@ public class MainActivity extends AppCompatActivity {
     private void previewPdf() {
         // Get the File location and file name.
         File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
-                "/Tes15.pdf");
+                "/Tes6.pdf");
         Log.d("pdfFIle", "" + file);
 
         // Get the URI Path of file.
